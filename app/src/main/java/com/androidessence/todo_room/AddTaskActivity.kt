c1205957941038b7525d4bafedc9163451a08e67
Anartz Mugika <mugan86@gmail.com>
2017-11-29 21:08:50 +0100

    Remove findViewById

--- app/src/main/java/com/androidessence/todo_room/AddTaskActivity.kt
+++ app/src/main/java/com/androidessence/todo_room/AddTaskActivity.kt
@@ -2,9 +2,9 @@ package com.androidessence.todo_room
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import android.widget.TextView
 import io.reactivex.Single
 import io.reactivex.schedulers.Schedulers
+import kotlinx.android.synthetic.main.activity_add_task.*
 
 
 class AddTaskActivity : AppCompatActivity() {
@@ -13,11 +13,8 @@ class AddTaskActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_task)
 
-        val editText = findViewById(R.id.task_description) as TextView
-        val submit = findViewById(R.id.submit)
-
         submit.setOnClickListener {
-            val description = editText.text.toString()
+            val description = task_description.text.toString()
 
             if (!description.isEmpty()) {
                 val task = Task(description)
